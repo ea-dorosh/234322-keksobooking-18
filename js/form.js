d9dcd3f7ddd051c71a86c1fb9f018ebc6ab7de1f
Egor <dorosh.ea@yandex.ru>
2019-10-13 18:05:23 +0400

    drag task ready

--- js/form.js
+++ js/form.js
@@ -9,7 +9,6 @@
   };
 
   var mapPinMain = document.querySelector('.map__pin--main');
-  var mapBlock = document.querySelector('.map');
   var advertForm = document.querySelector('.ad-form');
 
   var fieldAddress = document.querySelector('#address');
@@ -17,13 +16,21 @@
   var filterForm = document.querySelector('.map__filters');
   var select = filterForm.querySelectorAll('select');
 
-  fieldAddress.value = Math.round(parseInt(mapPinMain.style.left, 10) - MainPinParams.WIDTH / 2) + ', ' + Math.round(parseInt(mapPinMain.style.top, 10) - MainPinParams.START_HEIGHT / 2);
+  fieldAddress.value = Math.round(parseInt(mapPinMain.style.left, 10) + MainPinParams.WIDTH / 2) + ', ' + Math.round(parseInt(mapPinMain.style.top, 10) + MainPinParams.START_HEIGHT / 2);
 
   function calculatePinCoords() {
-    fieldAddress.value = Math.round(parseInt(mapPinMain.style.left, 10) - MainPinParams.WIDTH / 2) + ', ' + (parseInt(mapPinMain.style.top, 10) - MainPinParams.HEIGHT);
+    fieldAddress.value = Math.round(parseInt(mapPinMain.style.left, 10) + MainPinParams.WIDTH / 2) + ', ' + (parseInt(mapPinMain.style.top, 10) + MainPinParams.HEIGHT);
   }
 
-  mapPinMain.addEventListener('mousedown', activatePage);
+  mapPinMain.addEventListener('mousedown', function (evt) {
+
+    if (window.data.map.classList.contains('map--faded')) {
+      activatePage();
+    } else {
+      window.drag.drag(evt, mapPinMain);
+    }
+
+  });
   mapPinMain.addEventListener('keydown', function (evt) {
     if (window.util.isEnterEvent(evt)) {
       activatePage();
@@ -31,7 +38,7 @@
   });
 
   function activatePage() {
-    mapBlock.classList.remove('map--faded');
+    window.data.map.classList.remove('map--faded');
     advertForm.classList.remove('ad-form--disabled');
     calculatePinCoords();
     enableForm();
@@ -59,4 +66,10 @@
   }
 
   disableForm();
+
+  window.form = {
+    mainPinParams: MainPinParams,
+    calculatePinCoords: calculatePinCoords
+  };
+
 })();
