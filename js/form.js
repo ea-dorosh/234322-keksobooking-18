9d42ade7e028746cb6e4149b069ee0d2bb852bf5
Egor <dorosh.ea@yandex.ru>
2019-10-31 17:05:09 +0400

    load task

--- js/form.js
+++ js/form.js
@@ -2,6 +2,8 @@
 
 (function () {
 
+  var URL = 'https://js.dump.academy/keksobooking/dat';
+
   var MainPinParams = {
     WIDTH: 65,
     HEIGHT: 81,
@@ -15,6 +17,19 @@
   var fieldset = document.querySelectorAll('fieldset');
   var filterForm = document.querySelector('.map__filters');
   var select = filterForm.querySelectorAll('select');
+
+  var errorTemplate = document.querySelector('#error').content.querySelector('.error');
+
+  var onError = function (message) {
+    var error = errorTemplate.cloneNode(true);
+    var errorMessage = error.querySelector('.error__message');
+    errorMessage.textContent = message;
+    window.data.map.append(error);
+  };
+
+  var onSuccess = function (data) {
+    window.pins.render(data);
+  };
 
   fieldAddress.value = Math.round(parseInt(mapPinMain.style.left, 10) + MainPinParams.WIDTH / 2) + ', ' + Math.round(parseInt(mapPinMain.style.top, 10) + MainPinParams.START_HEIGHT / 2);
 
@@ -42,7 +57,7 @@
     advertForm.classList.remove('ad-form--disabled');
     calculatePinCoords();
     enableForm();
-    window.pins.render(window.data.adverts);
+    window.load.download(URL, onSuccess, onError);
   }
 
   function disableForm() {
