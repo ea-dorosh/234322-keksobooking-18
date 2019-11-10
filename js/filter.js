618d1d9cdb18d5772071194f087ab6af3dfd2197
Egor <dorosh.ea@yandex.ru>
2019-11-10 18:45:33 +0400

    fix

--- js/filter.js
+++ js/filter.js
@@ -5,43 +5,43 @@
   var adverts = [];
 
   function addFilter() {
-    filterQuantity();
+    window.pins.remove();
+    window.pins.render(filterQuantity(window.filter.adverts));
+
     advertType.addEventListener('change', filterType);
   }
 
-  function filterQuantity() {
+  function filterQuantity(array) {
     // eslint-disable-next-line consistent-return
-    var newAdverts = window.filter.adverts.filter(function (advert, index) {
+    var shortAdverts = array.filter(function (advert, index) {
       if (index < 5) {
         return advert;
       }
     });
-
-    window.pins.remove();
-    window.pins.render(newAdverts);
+    return shortAdverts;
   }
 
   function filterType() {
-    var newAdverts = window.filter.adverts;
+    var typeAdverts = window.filter.adverts;
     if (advertType.value === 'house') {
-      newAdverts = window.filter.adverts.filter(function (advert) {
+      typeAdverts = window.filter.adverts.filter(function (advert) {
         return advert.offer.type === 'house';
       });
     } else if (advertType.value === 'bungalo') {
-      newAdverts = window.filter.adverts.filter(function (advert) {
+      typeAdverts = window.filter.adverts.filter(function (advert) {
         return advert.offer.type === 'bungalo';
       });
     } else if (advertType.value === 'flat') {
-      newAdverts = window.filter.adverts.filter(function (advert) {
+      typeAdverts = window.filter.adverts.filter(function (advert) {
         return advert.offer.type === 'flat';
       });
     } else if (advertType.value === 'palace') {
-      newAdverts = window.filter.adverts.filter(function (advert) {
+      typeAdverts = window.filter.adverts.filter(function (advert) {
         return advert.offer.type === 'palace';
       });
     }
     window.pins.remove();
-    window.pins.render(newAdverts);
+    window.pins.render(filterQuantity(typeAdverts));
   }
 
   window.filter = {
