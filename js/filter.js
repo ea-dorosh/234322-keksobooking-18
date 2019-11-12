28d7033eb6c12cff54fc7d298f3f548457cf6668
Egor <dorosh.ea@yandex.ru>
2019-11-12 11:12:35 +0400

    done most of filters

--- js/filter.js
+++ js/filter.js
@@ -38,21 +38,42 @@
     });
   }
 
+  function filterByGuests() {
+    if (filtersState['housing-guests'] === 'any') {
+      return filterAdverts;
+    }
+    return filterAdverts.filter(function (advert) {
+      return advert.offer.guests === parseInt(filtersState['housing-guests'], 10);
+    });
+  }
+
+  function filterByPrice() {
+    if (filtersState['housing-price'] === 'any') {
+      return filterAdverts;
+    }
+    return filterAdverts.filter(function (advert) {
+      switch (filtersState['housing-price']) {
+        case 'middle':
+          return advert.offer.price > 10000 && advert.offer.price < 50000;
+        case 'low':
+          return advert.offer.price < 10000;
+        case 'high':
+          return advert.offer.price > 50000;
+      }
+      return advert.offer.price === filtersState['housing-price'];
+    });
+  }
+
 
   function onFilterChange(evt) {
     filterAdverts = adverts;
     window.pins.remove();
 
-    Object.keys(filtersState).forEach(function (value) {
-      filtersState[value] = evt.target.value;
-    });
-
-    //filtersState[evt.target.id] = evt.target.value;
-    filterAdverts = filterByType();
-
-    //filtersState[evt.target.id] = evt.target.value;
+    filtersState[evt.target.id] = evt.target.value;
     filterAdverts = filterByRoomQuantity();
-
+    filterAdverts = filterByType();
+    filterAdverts = filterByPrice();
+    filterAdverts = filterByGuests();
     window.pins.render(filterAdverts);
 
   }
